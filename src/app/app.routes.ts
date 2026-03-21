033c1d2d4aa29f502cd639f71fa04fb758bb3cf6
angelmartinezcastrojs-dev <angel.martinez.castrojs@gmail.com>
2026-03-20 18:36:23 -0600

    reajuste de portafolio y cracion de componente de experencia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { AboutMe } from './pages/about-me/about-me';
+import { Experience } from './pages/experience/experience';
 
-export const routes: Routes = [{
-    path: '',
-    component: AboutMe
-}];
+export const routes: Routes = [
+        {
+            path: '',
+            component: AboutMe
+        },
+        {
+            path: 'experience',
+            component: Experience
+        }
+    ];
